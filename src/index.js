5d54dd14e8f404247f8d451b88ab3ee1fc3afa25
BAravind85 <arubrp09@gmail.com>
2022-06-03 08:05:47 +0530

    moviesandfilms

--- src/index.js
+++ src/index.js
@@ -13,4 +13,33 @@ app.use('/', route);
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
+app.get('/sol1', function(req, res) {
+    let arr = [1, 2, 3, 5, 6, 7];
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i];
+    }
 
+    let n = arr[arr.length - 1]
+    let miss = n * (n + 1) / 2
+    let missingNumber = miss - sum;
+
+
+    res.send({ data: missingNumber });
+});
+app.get("/sol2", function(req, res) {
+    let arr = [33, 34, 35, 37, 38];
+    let n = arr.length + 1,
+        sum = 0;
+
+    for (var i in arr) {
+        sum += arr[i];
+    }
+
+    let firstDigit = arr[0]
+    let lastDigit = arr.pop()
+    let consecutiveSum = n * (firstDigit + lastDigit) / 2
+    let missingNumber = consecutiveSum - sum
+
+    res.send({ data: missingNumber });
+});
--- src/routes/route.js
+++ src/routes/route.js
@@ -4,37 +4,86 @@ const underscore = require('underscore')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
+router.get('/test-me', function(req, res) {
     myHelper.printDate()
     myHelper.getCurrentMonth()
     myHelper.getCohortData()
-    let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-    console.log('The first element received from underscope function is '+firstElement)
+    let firstElement = underscore.first(['Sabiha', 'Akash', 'Pritesh'])
+    console.log('The first element received from underscope function is ' + firstElement)
     res.send('My first ever api!')
 });
 
-router.get('/hello', function (req, res) {
-   
+router.get('/hello', function(req, res) {
+
     res.send('Hello there!')
 });
 
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
+router.get('/candidates', function(req, res) {
+    console.log('Query paramters for this request are ' + JSON.stringify(req.query))
     let gender = req.query.gender
     let state = req.query.state
     let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
+    console.log('State is ' + state)
+    console.log('Gender is ' + gender)
+    console.log('District is ' + district)
+    let candidates = ['Akash', 'Suman']
     res.send(candidates)
 })
 
-router.get('/candidates/:canidatesName', function(req, res){
-    console.log('The request objects is '+ JSON.stringify(req.params))
-    console.log('Candidates name is '+req.params.canidatesName)
+router.get('/candidates/:canidatesName', function(req, res) {
+    console.log('The request objects is ' + JSON.stringify(req.params))
+    console.log('Candidates name is ' + req.params.canidatesName)
     res.send('Done')
 })
+
+
+
+let moVie = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+router.get('/movies', function(req, res) {
+    console.log(moVie)
+    res.send('Done')
+});
+router.get('/movies/:number', function(req, res) {
+    let number = req.params.number
+    if (number < moVie.length) {
+
+        res.send(moVie[number])
+    } else {
+        res.send('No movie exist with that index number')
+    }
+
+
+});
+const films = [{
+        'id': 1,
+        'name': 'The Shining'
+    },
+    {
+        'id': 2,
+        'name': 'Incendies'
+    },
+    {
+        'id': 3,
+        'name': 'Rang de Basanti'
+    },
+    {
+        'id': 4,
+        'name': 'Finding Nemo'
+
+    }
+]
+router.get('/films', function(req, res) {
+    res.send(films)
+});
+router.get('/films/:filmsid', function(req, res) {
+    let id = req.params.filmsid
+    if (id < films.length && id > 0) {
+        res.send(films[id - 1]);
+    } else {
+        res.send('Id doesnt exist in films list')
+    }
+
+});
 
 
 module.exports = router;
